0076dd849f56d507e1d199c7d07bbc989663809e
DimitrovVladimir <vmdimitrow@gmail.com>
2022-11-07 16:20:22 +0200

    2loop

---
+++ BPM-Counter/BPM-Counter.cs
@@ -0,0 +1,19 @@
+﻿namespace BPM_Counter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var bpm =double.Parse(Console.ReadLine());
+            var beats = double.Parse(Console.ReadLine());
+            double bars = Math.Round(beats / 4.0, 1);
+
+            double secondsForOneBeat = 60 / bpm;
+            double totalSeconds = secondsForOneBeat * beats;
+            int minutes = (int)totalSeconds/60;
+            int seconds = (int)totalSeconds % 60;
+            Console.WriteLine($"{bars} bars - {minutes}m {seconds}s");
+
+        }
+    }
+}
---
+++ SMS-typing/SMS-typing.cs
@@ -0,0 +1,35 @@
+﻿namespace SMS_typing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string message = String.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                string digits = Console.ReadLine();
+            int mainDigit = int.Parse(digits[0].ToString());
+                int offset = (mainDigit -2) * 3;
+
+                if(mainDigit == 8 || mainDigit == 9)
+                {
+                    offset += 1;
+
+                }
+                int letterIndex = offset +digits.Length-1;
+                if (mainDigit == 0)
+                {
+                    message += "";
+                }
+                else
+                {
+                    message += (char)(97+letterIndex);
+                }
+
+               
+            }
+            Console.WriteLine(message);
+        }
+    }
+}
